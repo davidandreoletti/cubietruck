1659c0dbd49a6f17136cd2de029215e23ad068e5
David Andreoletti <david.andreoletti.dev@gmail.com>
2013-12-09 21:52:02 +0800

    Moved serial device setup

--- manualbuild-console-serial.sh
+++ manualbuild-console-serial.sh
@@ -1,4 +1,8 @@
-SERIAL_DEVICE=${SERIAL_DEVICE:-"/dev/ttyUSB0"}
+SERIAL_DEVICE=${SERIAL_DEVICE:-"/dev/ttyUSB0"} # Assuming Prolific PL2303 like chip
+
+echo "Set write permission (temporary)"
+sudo chmod 666 ${SERIAL_DEVICE}
+ls -l ${SERIAL_DEVICE}
 
 echo "Turning off output hardware flow control on ${SERIAL_DEVICE} - No RTS and no CTS wires on PL2303 cables."
 # See also http://www.tldp.org/HOWTO/Serial-HOWTO-4.html"
