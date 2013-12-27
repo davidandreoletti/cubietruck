918bc54846121d36b6efd628ee3d112530ed1e8f
David Andreoletti <david.andreoletti.dev@gmail.com>
2013-12-27 08:41:07 +0800

    Added support for LEDs control/triggers Added support for custom kernel config (enabled by default) Added wifi interface settings (dummy configuration) Added support for locales (default - US english) Documented existing 2GB RAM support

--- customize-mounted-rootfs.sh
+++ customize-mounted-rootfs.sh
@@ -19,19 +19,29 @@ cp /usr/bin/qemu-arm-static usr/bin/
 mv etc/resolv.conf etc/resolv.conf.saved
 cp /etc/resolv.conf etc/resolv.conf
 
+rsync --ignore-times -rv --chmod=ugo=rwX "${ROOTFS_CUSTOM_DIR}/" "${MNT_ROOTFS_DIR}" 
+
 # Mount sys, proc and dev
 for m in `echo 'sys dev proc'`; do sudo mount /$m ./$m -o bind; done
 
 # chroot into your target filesystem
 sudo LC_ALL=C chroot . /bin/bash -x <<'EOF'
+# Install custom startup scripts
+chmod +x /etc/init.d/host/cubian-*
+update-rc.d cubian-firstrun defaults
 # Get and install some packages
 apt-get update
 #apt-get -y install openssh-server vim
+apt-get -y install locales
+dpkg-reconfigure locales
+export LANG=en_US.UTF-8
 apt-get -y install openssh-server openssh-client vim wireless-tools wpasupplicant hwinfo
 apt-get -y install rsync duplicity 
 apt-get -y install cpufrequtils
 apt-get -y install man
 apt-get -y install ntp
+apt-get -y install udev
+apt-get -y install tree 
 # Set timezone
 mv -v etc/localtime etc/localtime.bkp
 #ln -s /usr/share/zoneinfo/Asia/Taipei /etc/localtime 
@@ -55,6 +65,6 @@ mv etc/resolv.conf.saved etc/resolv.conf
 
 cd -
 
-rsync --ignore-times -rv --chmod=ugo=rwX "${ROOTFS_CUSTOM_DIR}/" "${MNT_ROOTFS_DIR}" 
+
 
 
